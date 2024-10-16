d0dccda92c48b79113bb0e41e189dd55c4891445
Jongchan_Choi <118791747+CJC0512@users.noreply.github.com>
2024-10-16 17:05:18 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,49 @@
+# def solution(n, lost, reserve):
+#     stu_list = []
+#     for i in range(1, n + 1):
+#         if i not in lost:
+#             stu_list.append(i)
+            
+#     print("stu", stu_list)
+            
+#     sorted_reserve = sorted(reserve)
+#     print("sor_re", sorted_reserve)
+        
+#     for rsv in sorted_reserve:
+#         print(rsv)
+#         if rsv not in stu_list:
+#             stu_list.append(rsv)
+#         else:
+#             if (rsv - 1) > 0 and ((rsv - 1) not in stu_list):
+#                 stu_list.append(rsv - 1)
+#             elif (rsv + 1) <= n and ((rsv + 1) not in stu_list):
+#                 stu_list.append(rsv + 1)
+#         print("stu", stu_list)
+
+#     return len(stu_list)
+
+
+
+def solution(n, lost, reserve):
+    stu_list = []
+    for i in range(1, n + 1):
+        if i not in lost:
+            stu_list.append(i)
+                
+    sorted_reserve = sorted(reserve)
+            
+    remain = []
+    for rsv in sorted_reserve:
+        print(rsv)
+        if rsv in lost:
+            stu_list.append(rsv)
+        else:
+            remain.append(rsv)
+        
+    for rem in remain:
+        if (rem - 1) > 0 and ((rem - 1) not in stu_list):
+            stu_list.append(rem - 1)
+        elif (rem + 1) <= n and ((rem + 1) not in stu_list):
+            stu_list.append(rem + 1)
+
+    return len(stu_list)
