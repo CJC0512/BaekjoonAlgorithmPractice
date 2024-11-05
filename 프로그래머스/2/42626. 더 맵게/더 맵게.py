7f57be6e8516f411dfa95300093482008e67606b
Jongchan_Choi <118791747+CJC0512@users.noreply.github.com>
2024-11-05 13:50:54 +0900

    [level 2] Title: 더 맵게, Time: 1931.20 ms, Memory: 49.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,16 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer = 0
+    
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        new_scoville = first + (second * 2)
+        heapq.heappush(scoville, new_scoville)
+        answer += 1
+    
+    return answer
